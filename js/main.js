e7eb9ca8319548e357fe24d397576b594097d150
Antti Kuru <antti.i.kuru@gmail.com>
2024-09-17 15:08:12 +0300

    made functionality for fetching the news from the handwritten JSON file

--- js/main.js
+++ js/main.js
@@ -10,3 +10,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const mobileMenu = document.querySelector(".mobile-menu")
     hamburgerButton.addEventListener("click", () => mobileMenu.classList.toggle('active'))
 })
+
+
+
+
+
+
+
+
+/* news page loading news from JSON file */
+
+
+async function getNewsData () {
+    const newsPromise = await fetch('data/news.json')
+    const newsJSON = await newsPromise.json()
+
+    const newsGrid = document.querySelector('.news-grid')
+    //console.log(newsJSON)
+    
+    newsJSON.forEach(news => {
+        let newsCard = document.createElement('div')
+        newsCard.classList.add('card')
+
+        newsCard.innerHTML = 
+        `   <img src="images/${news.image}" alt="${news.title}">
+            <h2 class=card-text text-l textcenter"> ${news.title}</h2>
+            <p> ${news.content}</p>
+            <p> <i class="fas fa-clock"></i> ${news.date}</p>
+        `;
+        newsGrid.appendChild(newsCard)
+        
+    })
+    
+    }
+
+
+getNewsData()
